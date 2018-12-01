7c4077f17bc3208577ff3e3845c345cea60dcf8a
ye0823 <ye880823@gmail.com>
2018-12-01 14:45:11 +0900

    news-service init

--- news-service/src/main/java/com/news/NewsApplication.java
+++ news-service/src/main/java/com/news/NewsApplication.java
@@ -3,14 +3,12 @@ package com.news;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableEurekaClient
-@EnableZuulProxy
 @SpringBootApplication
-public class GatewayApplication {
+@EnableEurekaClient
+public class NewsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
+		SpringApplication.run(NewsApplication.class, args);
 	}
 }
---
+++ news-service/src/main/java/com/news/svc/impl/NewsSVCImpl.java
@@ -0,0 +1,10 @@
+package com.news.svc.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.news.svc.NewsSVC;
+
+@Service
+public class NewsSVCImpl implements NewsSVC{
+
+}
