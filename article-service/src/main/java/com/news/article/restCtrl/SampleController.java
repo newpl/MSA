ad1ba8157a4bdc6ae3aec013a19a93df7e759ad9
ye0823 <ye880823@gmail.com>
2018-12-02 14:40:20 +0900

    article init

--- article-service/src/main/java/com/news/article/restCtrl/SampleController.java
+++ article-service/src/main/java/com/news/article/restCtrl/SampleController.java
@@ -1,13 +1,10 @@
 package com.news.article.restCtrl;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.news.restapi.ArticleServiceAPI;
 
 @RestController
 @RequestMapping("/test")
@@ -15,6 +12,7 @@ public class SampleController {
 	
 //	@Autowired
 //	ArticleServiceAPI articleServiceAPI;
+	
 	@GetMapping("/test1")
 	public ResponseEntity test(){
 //		articleServiceAPI.test();
