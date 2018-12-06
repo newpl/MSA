aece559c1528464d27a424558971e8256bc06018
ye0823 <ye880823@gmail.com>
2018-12-06 21:40:40 +0900

    liquibase 추가 후 article 관련 테이블 create

--- article-service/src/main/java/com/news/article/ArticleApplication.java
+++ article-service/src/main/java/com/news/article/ArticleApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 @SpringBootApplication
 @EnableEurekaClient
+@EnableEncryptableProperties
 public class ArticleApplication {
 
 	public static void main(String[] args) {
