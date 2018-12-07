2913a273c0ab4cf4c497999fe53ac3ba82ba5747
kja66 <kja66@DESKTOP-0FKNSU1>
2018-12-07 20:38:58 +0900

    pom추가, model, repo패키지 생성

---
+++ article-service/src/main/java/com/news/article/repo/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.news.article.repo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.news.article.model.Customer;
+
+
+public interface CustomerRepository extends JpaRepository<Customer, Long>{
+	List<Customer> findByLastName(String lastName);
+	List<Customer> findByIdBetween(Long id1, Long id2);
+	
+}
